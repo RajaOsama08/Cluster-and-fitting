8cdb6a12ce198aeb1d42278a9292bc033d26f3f1
Raja Osama <therajaosama08@gmail.com>
2023-01-19 09:32:57 +0000

    read fun

--- ADS_assng3.py
+++ ADS_assng3.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 
-# read world bank file 
-pd_df = pd.read_csv("data.csv", skiprows=3)
-print('world bank', pd_df)
+
+def read_data(file):
+
+    pd_df = pd.read_csv(file, skiprows=3)
+    pd_df = pd_df.drop(["Unnamed: 66"], axis=1)
+    return pd_df, pd_df.T
+
+
+file = 'data.csv'
+pd_df = read_data(file)
+print(pd_df)
